31aaf41b0bbaecd91e1d180c20f5174a2ae22d13
七种文学 <3117965903@qq.com>
2024-05-26 15:54:46 +0800

    初步更改，完成大部分

--- README.md
+++ README.md
@@ -7,12 +7,8 @@ web前端个人简历：一个追梦在前端路上的小白
 - particles.js粒子插件
 - Handlebars.js模板引擎
 
-
 #### 在线预览
-[web前端个人简历：一个追梦在前端路上的小白](http://wttandroid.gitee.io/wttandroid.github.io/)
-
-#### 源码
-- 后台管理：+wx:wtt1457321681
+[web前端个人简历：一个追梦在前端路上的小白]()
 
 #### 使用说明
 1.  修改src/config.js内容作为配置文件
@@ -21,8 +17,8 @@ web前端个人简历：一个追梦在前端路上的小白
 export default {
   // 首页部分
   page1: {
-    titleEn:'Hello,I`m WeiTingting', //英文标题
-    title: '一个追梦在前端路上的小白',//中文标题
+    titleEn:'Hello,I`m QZWX', //英文标题
+    title: '一个追梦的小白',//中文标题
   },
   // About Me部分
   page2:{
@@ -71,7 +67,7 @@ export default {
     github:'...',//github链接
     bili:'...',//B站链接
     email:'Mailto:xxx@qq.com?Subject=邮箱标题&Body=邮箱内容！',//email
-    zhihu:'...',//知乎链接
+    douyin:'...',//抖音链接
     weixin:"<img style='width:120px' src='...' alt='加载失败'>",//微信弹窗
     qq:"<img style='width:120px' src='...' alt='加载失败'>"//QQ弹窗
   }
@@ -80,13 +76,3 @@ export default {
 
 2.  运行index.html文件访问本地服务  (！！注意：因为file跨域问题，本地运行必须以服务器方式启动项目！)
 
-
-### 截图预览
-
-![首页](https://images.gitee.com/uploads/images/2020/0722/194832_8b05a480_5641265.png "1.png")![About Me](https://images.gitee.com/uploads/images/2020/0722/194851_9fc98b82_5641265.png "2.png")![My Services](https://images.gitee.com/uploads/images/2020/0722/194909_f35f4738_5641265.png "3.png")![My Mastering](https://images.gitee.com/uploads/images/2020/0722/195119_1e54b805_5641265.png "4.png")![My Production](https://images.gitee.com/uploads/images/2020/0722/194926_268983f1_5641265.png "5.png")![结尾页](https://images.gitee.com/uploads/images/2020/0722/194947_d623e840_5641265.png "6.png")
-![后台](src/img/%E5%BE%AE%E4%BF%A1%E6%88%AA%E5%9B%BE_20230707225052.png)
-![后台](src/img/%E5%BE%AE%E4%BF%A1%E6%88%AA%E5%9B%BE_20230707225113.png)
-
-
-#### 联系我：远程调试/二次开发/毕设
-![远程调试/二次开发/毕设](src/img/%E6%97%A0%E6%A0%87%E9%A2%98.png)
--- src/config.js
+++ src/config.js
@@ -1,127 +1,154 @@
 export default {
   // page1部分
   page1: {
-    titleEn: 'Hello,I`m WeiTingting', //英文标题
-    title: '一个追梦在前端路上的小白', //中文标题
+    titleEn: "Hello,I`m QZWX", //英文标题
+    title: "一个追梦的小白", //中文标题
   },
   // page2部分
   page2: {
-    authorImg: 'page1.jpg', // 作者头像
-    xinhui: '我叫卫婷婷，是一名在读的大三学生。', // 幸会
-    qiuzhi: '前端开发', // 求职意向
-    guanyuwo: '掌握了前端js、html、css基本技术和Vue框架、UI框架，nodejs等等，有着公司项目实战的经验和一些java安卓开发经历。希望可以和大家一起学习，共同进步！' // 关于我
+    authorImg: "七种文学logo.png", // 作者头像
+    xinhui: "我叫七种文学，是一名在读的大二学生。", // 幸会
+    qiuzhi: "腾讯公司CEO", // 求职意向
+    guanyuwo: "能走路，会刷抖音，能玩游戏【真实情况这里写人生经历】", // 关于我
   },
   // page3部分
-  page3: [{
-    icon: 'icon-tubiao-',
-    title: 'UI设计',
-    msg: ['PS切图、界面排版']
-  },{
-    icon: 'icon-diannao', // 图标
-    title: '网页制作', // 标题
-    msg: ['响应式页面', 'css、scss动画效果',] //介绍
-  }, {
-    icon: 'icon-qianbi1',
-    title: '前端功能',
-    msg: ['JS完成常见的交互功能', '用AJAX读取后台数据且渲染']
-  },  {
-    icon: 'icon-shouji',
-    title: '框架',
-    msg: ['vue框架','UI框架、express框架、mysql',]
-  }],
+  page3: [
+    {
+      icon: "icon-tubiao-",
+      title: "UI设计",
+      msg: ["PS切图、界面排版"],
+    },
+    {
+      icon: "icon-diannao", // 图标
+      title: "视频剪辑", // 标题
+      msg: ["动画特效", "视频剪辑"], //介绍
+    },
+    {
+      icon: "icon-qianbi1",
+      title: "前端功能",
+      msg: ["JS完成常见的交互功能", "用AJAX读取后台数据且渲染"],
+    },
+    {
+      icon: "icon-shouji",
+      title: "框架",
+      msg: ["vue框架", "UI框架、express框架、mysql"],
+    },
+  ],
   // page4部分
   page4: {
     // 我的历程
-    course: [{
-      date: '2020/9——至今', // 时间
-      desc: { // 经历
-        title: '山西传媒学院',
-        list: ['山西省职业技能大赛web前端开发一等奖', '专升本上岸', '软件开发公司实习']
-      }
-    }, {
-      date: '2019/9——2020/9',
-      desc: {
-        title: '晋城职业技术学院',
-        list: ['学习前端语言', '构建页面 / 利用JS、CSS3等制作网站，vue有关项目', '维护508工作室','获得Web前段开发1+X证书','普通话二级甲等','国家励志奖学金']
-      }
-    }, {
-      date: '2018/9——2019/9',
-      desc: {
-        title: '晋城职业技术学院',
-        list: ['学习java语言', '利用Android、sql等制作app', '参加山西省职业院校移动互联技能大赛', '全国计算机等级二级证书', '国家励志奖学金']
-      }
-    }],
+    course: [
+      {
+        date: "2022/9——至今", // 时间
+        desc: {
+          // 经历
+          title: "太原工业学院",
+          list: ["获得了XX奖", "获得XX荣誉", "XX公司实习经历"],
+        },
+      },
+      {
+        date: "2019/9——2020/9",
+        desc: {
+          title: "湖北武汉",
+          list: [
+            "学习前端语言",
+            "构建页面 / 利用JS、CSS3等制作网站，vue有关项目",
+            "获得XX证书",
+          ],
+        },
+      },
+      {
+        date: "2018/9——2019/9",
+        desc: {
+          title: "XX地区",
+          list: ["学习XX", "制作XXAPP", "参加XX大赛", "获得XX荣誉", "XX成就"],
+        },
+      },
+    ],
     // 我的拓展技能掌握
-    singlelist: [{
-      title: 'bootsrap',
-      text: '了解'
-    }, {
-      title: 'Element-ui',
-      text: '掌握'
-    }, {
-      title: 'mint-ui',
-      text: '掌握'
-    }, {
-      title: 'scss',
-      text: '掌握'
-    }],
+    singlelist: [
+      {
+        title: "APP开发",
+        text: "了解",
+      },
+      {
+        title: "AE",
+        text: "掌握",
+      },
+      {
+        title: "剪映",
+        text: "掌握",
+      },
+      {
+        title: "XX专业",
+        text: "掌握",
+      },
+    ],
     // 我的基本技能掌握
-    proresslist: [{
-      title: 'HTML、CSS、SCSS',
-      value: '90%'
-    }, {
-      title: 'JS、JQ',
-      value: '90%'
-    }, {
-      title: 'Vue',
-      value: '90%'
-    }, {
-      title: 'NodeJs',
-      value: '80%'
-    }, {
-      title: 'sql',
-      value: '80%'
-    }]
+    proresslist: [
+      {
+        title: "HTML、CSS、SCSS",
+        value: "90%",
+      },
+      {
+        title: "JS、JQ",
+        value: "90%",
+      },
+      {
+        title: "Vue",
+        value: "90%",
+      },
+      {
+        title: "NodeJs",
+        value: "80%",
+      },
+      {
+        title: "sql",
+        value: "80%",
+      },
+    ],
   },
-  // page5部分
-  page5: [{
-    title: '508工作室',
-    content: '晋城职业技术学院508工作室独立站点的维护',
-    image: 'box1.png',
-    href: 'https://lab508.gitee.io/'
-  }, {
-    title: '图书管理系统',
-    content: 'Vue,NodeJs图书管理系统前后端',
-    image: 'box3.jpg',
-    href: 'https://gitee.com/wttAndroid/book_admin'
-  }, {
-    title: '校园约吧',
-    content: 'Vue,NodeJs移动端校园项目前后端',
-    image: 'box5.jpg',
-    href: 'https://gitee.com/wttAndroid/xyy_server'
-  }, {
-    title: '蘑菇街',
-    content: 'Vue蘑菇街商城案例前端',
-    image: 'box2.png',
-    href: 'https://github.com/wttAndroid/MyShoppingStreet'
-  }, {
-    title: '个人简历',
-    content: 'html,css,jq,bootsrap搭建个人简历网站',
-    image: 'box4.png',
-    href: ''
-  }, {
-    title: '二维码生成器',
-    content: 'Android二维码生成器',
-    image: 'box6.jpeg',
-    href: ''
-  }],
+  // page5部分     herf放你需要跳转的连接
+  page5: [
+    {
+      title: "XX项目",
+      content: "用了XX方式创造出XX作品合集",
+      image: "box3.jpg",
+      href: "",
+    },
+    {
+      title: "音乐二创",
+      content: "利用AU等软件创作改编出的音乐合集",
+      image: "box5.jpg",
+      href: "",
+    },
+    {
+      title: "视频创作",
+      content: "用AE，剪映等软件所创作的视频合集",
+      image: "壁纸1.png",
+      href: "",
+    },
+    {
+      title: "个人简历",
+      content: "html,css,jq,bootsrap搭建个人简历网站合集",
+      image: "box4.png",
+      href: "",
+    },
+    {
+      title: "开发的APP",
+      content: "自主钻研利用Androidstudio开发软合集",
+      image: "box6.jpeg",
+      href: "",
+    },
+  ],
   // page6部分
   page6: {
-    github: 'https://github.com/wttAndroid',
-    bili: '',
-    email: 'Mailto:1457321681@qq.com?Subject=邮箱标题&Body=邮箱内容！',
-    zhihu: '',
-    weixin: "<img style='width:120px' src='http://wttandroid.gitee.io/wttandroid.github.io/src/img/weix.jpg' alt='加载失败'>",
-    qq: "<img style='width:120px' src='http://wttandroid.gitee.io/wttandroid.github.io/src/img/qq.jpg' alt='加载失败'>"
-  }
-}
+    github: "https://github.com/7qzwx",
+    bili: "https://space.bilibili.com/397437067",
+    email: "Mailto:3117965903@qq.com?Subject=邮箱标题&Body=邮箱内容！",
+    douyin: "https://www.douyin.com/user/self",
+    weixin:
+      "<img style='width:120px' src='../src/img/七种文学QQ二维码.jpg' alt='wx二维码'>",
+    qq: "<img style='width:120px' src='../src/img/七种文学QQ二维码.jpg' alt='QQ二维码'>",
+  },
+};
